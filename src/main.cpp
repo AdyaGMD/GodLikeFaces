05c17f729ff7cdfa391cd2a096a1c76f479c0f5b
Adya <114347520+AdyaGMD@users.noreply.github.com>
2024-06-17 21:10:54 +0100

    removed feature because of regression

--- src/main.cpp
+++ src/main.cpp
@@ -44,46 +44,46 @@ class $modify(GJDifficultySprite) {
 	}
 };
 
-class $modify(RateStarsLayer) {
+// class $modify(RateStarsLayer) {
 
-	bool enable = true;
+// 	bool enable = true;
 
-	bool init(int a, bool b, bool c) {
-		auto result = RateStarsLayer::init(a, b, c);
-		if (b == false && c == false) enable = false;
-		return result;
-	}
+// 	bool init(int a, bool b, bool c) {
+// 		auto result = RateStarsLayer::init(a, b, c);
+// 		if (b == false && c == false) enable = false;
+// 		return result;
+// 	}
 
-	void onFeature(CCObject* obj) {
-		bool wasFaceModified = false;
-		if (_featureState > 2) wasFaceModified = true;
-		RateStarsLayer::onFeature(obj);
-		if (wasFaceModified) {
-			_diffSprite->updateDifficultyFrame(_difficulty, static_cast<GJDifficultyName>(_demon));
-		}
-		if (_difficulty == 6) {
-			_demon = 1;
-			(_featureState > 2) ? _diffSprite->setPositionY(20.950f) : _diffSprite->setPositionY(25.950f);
-		} else {
-			_demon = 0;
-			_diffSprite->setPositionY(25.950f);
-		}
-		return;
-	}
-	void selectRating(CCObject* obj) {
-		RateStarsLayer::selectRating(obj);
-		if (!enable) return;
-		if (_difficulty == 6) {
-			_demon = 1;
-			if (_featureState > 2)
-			_diffSprite->setPositionY(20.950f);
-		} else {
-			_demon = 0;
-			_diffSprite->setPositionY(25.950f);
-		}
-		if (_featureState > 2) {
-			specialTexturesCheck(_diffSprite);
-		}
-		return;
-	}
-};
+// 	void onFeature(CCObject* obj) {
+// 		bool wasFaceModified = false;
+// 		if (_featureState > 2) wasFaceModified = true;
+// 		RateStarsLayer::onFeature(obj);
+// 		if (wasFaceModified) {
+// 			_diffSprite->updateDifficultyFrame(_difficulty, static_cast<GJDifficultyName>(_demon));
+// 		}
+// 		if (_difficulty == 6) {
+// 			_demon = 1;
+// 			(_featureState > 2) ? _diffSprite->setPositionY(20.950f) : _diffSprite->setPositionY(25.950f);
+// 		} else {
+// 			_demon = 0;
+// 			_diffSprite->setPositionY(25.950f);
+// 		}
+// 		return;
+// 	}
+// 	void selectRating(CCObject* obj) {
+// 		RateStarsLayer::selectRating(obj);
+// 		if (!enable) return;
+// 		if (_difficulty == 6) {
+// 			_demon = 1;
+// 			if (_featureState > 2)
+// 			_diffSprite->setPositionY(20.950f);
+// 		} else {
+// 			_demon = 0;
+// 			_diffSprite->setPositionY(25.950f);
+// 		}
+// 		if (_featureState > 2) {
+// 			specialTexturesCheck(_diffSprite);
+// 		}
+// 		return;
+// 	}
+// };
